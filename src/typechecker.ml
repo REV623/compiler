69ff275791fb888fac86c5afb6f0b96ec2b71614
REV623 <jedsadakorn_k@cmu.ac.th>
2024-02-19 00:40:07 +0700

    update

--- src/typechecker.ml
+++ src/typechecker.ml
@@ -26,6 +26,7 @@ type ty  = Integer | Bool | Double | Char | String
     data : ty;
   }
   | Unit
+  | EmptyList
 
 type binding = 
   | Map of {
@@ -52,6 +53,7 @@ let rec string_of_type(t: ty) : string =
   | Data r -> r.name ^ (let rec str_loop tv_list : string = match tv_list with | hd::tl -> string_of_type hd ^ " " ^ str_loop tl | [] -> "" in str_loop r.tv_list)
   | Constructor r -> r.name
   | Unit -> "()"
+  | EmptyList -> "[]"
 
 (* TODO: define your own type checking *)
 let rec typecheck_prog(p : unit prog) : ty prog option =
@@ -91,22 +93,47 @@ and typecheck_defn(p : unit prog)(env : binding list) : ty prog option =
   match p with
   | DataDef _r -> None
   | TypeDef _r -> None
-  | FuncDef r -> (match typecheck_expr r.expr env with
-      | Some expr' -> Some (FuncDef {r with 
-          prop = Unit;
-          arg_list = expr'::[];
-          return_type = expr';
-          expr = expr';
-        })
-      | _ -> None
-    )
+  | FuncDef r -> (
+    match typecheck_expr r.expr env with
+    | Some expr' -> Some (FuncDef {r with 
+        prop = Unit;
+        arg_list = expr'::[];
+        return_type = expr';
+        expr = expr';
+      })
+    | _ -> None
+  )
   | _ -> None
 and typecheck_expr(p : unit prog)(env : binding list) : ty prog option =
   match p with
   | IntLit r -> Some (IntLit {r with prop = Integer})
+  | FloatLit r -> Some (FloatLit {r with prop = Double})
   | BoolLit r -> Some (BoolLit {r with prop = Bool})
   | CharLit r -> Some (CharLit {r with prop = Char})
+  | StringLit r -> Some (StringLit {r with prop = String})
   | Ident r -> Some (Ident {r with prop = find_binding_type env r.name})
+  | Unit _r -> Some (Unit {prop = Unit})
+  | EmptyList _r -> Some (EmptyList {prop = List EmptyList}) (* prop = List EmptyList ??? *)
+  | UnaExpr ({op = Sub; _} as r) -> (
+    match typecheck_expr r.expr env with
+    | Some expr' -> (
+        match prop_of_prog expr' with
+        | Integer -> Some (UnaExpr {r with
+            prop = Integer;
+            expr = expr';
+          })
+        | Double -> Some (UnaExpr {r with
+            prop = Double;
+            expr = expr';
+          })
+        | t -> prerr_string
+            (string_of_positions r.pos 
+              ^ ": expected Integer or Double operands, but found "
+              ^ string_of_type t ^ "\n");
+            None
+      )
+    | _ -> None
+  )
   | BinExpr ({op = Add; _} as r)
   | BinExpr ({op = Sub; _} as r)
   | BinExpr ({op = Mult; _} as r) -> (
@@ -121,40 +148,57 @@ and typecheck_expr(p : unit prog)(env : binding list) : ty prog option =
         | (Double, Double)
         | (Integer, Double)
         | (Double, Integer) -> Some (BinExpr {r with
-          prop = Double;
-          left = left';
-          right = right';
-        })
+            prop = Double;
+            left = left';
+            right = right';
+          })
         | (t1, t2) -> prerr_string
             (string_of_positions r.pos 
               ^ ": expected int operands, but found "
               ^ string_of_type t1 ^ ", "
               ^ string_of_type t2 ^ "\n");
-          None
-    )
+            None
+      )
+    | _ -> None
+  )
+  | BinExpr ({op = Div; _} as r) -> (
+    match (typecheck_expr r.left env, typecheck_expr r.right env) with
+    | (Some left', Some right') -> (
+        match (prop_of_prog left', prop_of_prog right') with
+        | (Integer, Integer)
+        | (Double, Double)
+        | (Integer, Double)
+        | (Double, Integer) -> Some (BinExpr {r with
+            prop = Double;
+            left = left';
+            right = right';
+          })
+        | (t1, t2) -> prerr_string
+            (string_of_positions r.pos 
+              ^ ": expected int operands, but found "
+              ^ string_of_type t1 ^ ", "
+              ^ string_of_type t2 ^ "\n");
+            None
+      )
     | _ -> None
   )
   | BinExpr ({op = IntDiv; _} as r) -> (
     match (typecheck_expr r.left env, typecheck_expr r.right env) with
     | (Some left', Some right') -> (
         match (prop_of_prog left', prop_of_prog right') with
-        | (Integer, Integer) -> Some (BinExpr {r with
+        | (Integer, Integer)
+        | (Double, Integer) -> Some (BinExpr {r with
             prop = Integer;
             left = left';
             right = right';
           })
-        | (Double, Integer) -> Some (BinExpr {r with
-          prop = Integer;
-          left = left';
-          right = right';
-        })
         | (t1, t2) -> prerr_string
             (string_of_positions r.pos 
               ^ ": expected int operands, but found "
               ^ string_of_type t1 ^ ", "
               ^ string_of_type t2 ^ "\n");
-          None
-    )
+            None
+      )
     | _ -> None
   )
   | BinExpr ({op = Mod; _} as r) -> (
@@ -167,70 +211,67 @@ and typecheck_expr(p : unit prog)(env : binding list) : ty prog option =
             right = right';
           })
         | (Double, Integer) -> Some (BinExpr {r with
-          prop = Double;
-          left = left';
-          right = right';
-        })
-        | (t1, t2) -> prerr_string
-            (string_of_positions r.pos 
-              ^ ": expected int operands, but found "
-              ^ string_of_type t1 ^ ", "
-              ^ string_of_type t2 ^ "\n");
-          None
-    )
-    | _ -> None
-  )
-  | BinExpr ({op = Div; _} as r) -> (
-    match (typecheck_expr r.left env, typecheck_expr r.right env) with
-    | (Some left', Some right') -> (
-        match (prop_of_prog left', prop_of_prog right') with
-        | (Integer, Integer) -> Some (BinExpr {r with
             prop = Double;
             left = left';
             right = right';
           })
-        | (Double, Double) -> Some (BinExpr {r with
-          prop = Double;
-          left = left';
-          right = right';
-        })
-        | (Integer, Double) -> Some (BinExpr {r with
-          prop = Double;
-          left = left';
-          right = right';
-        })
-        | (Double, Integer) -> Some (BinExpr {r with
-          prop = Double;
-          left = left';
-          right = right';
-        })
         | (t1, t2) -> prerr_string
             (string_of_positions r.pos 
               ^ ": expected int operands, but found "
               ^ string_of_type t1 ^ ", "
               ^ string_of_type t2 ^ "\n");
-          None
-    )
+            None
+      )
     | _ -> None
   )
-  | UnaExpr ({op = Sub; _} as r) -> (
-    match (typecheck_expr r.expr env) with
-    | (Some right') -> (
-        match (prop_of_prog right') with
-        | (Integer) -> Some (UnaExpr {r with
-            prop = Integer;
-            expr = right';
+  | BinExpr ({op = Less; _} as r)
+  | BinExpr ({op = Leq; _} as r)
+  | BinExpr ({op = Geq; _} as r)
+  | BinExpr ({op = Greater; _} as r) -> (
+    match (typecheck_expr r.left env, typecheck_expr r.right env) with
+    | (Some left', Some right') -> (
+        match (prop_of_prog left', prop_of_prog right') with
+        | (Integer, Integer)
+        | (Double, Double)
+        | (Integer, Double)
+        | (Double, Integer) -> Some (BinExpr {r with
+            prop = Bool;
+            left = left';
+            right = right';
           })
-        | (Double) -> Some (UnaExpr {r with
-          prop = Double;
-          expr = right';
-        })
-        | (t1) -> prerr_string
+        | (t1, t2) -> prerr_string
             (string_of_positions r.pos 
               ^ ": expected bool operands, but found "
-              ^ string_of_type t1 ^ "\n");
-          None
-    )
+              ^ string_of_type t1 ^ ", "
+              ^ string_of_type t2 ^ "\n");
+            None
+      )
+    | _ -> None
+  )
+  | BinExpr ({op = Equal; _} as r)
+  | BinExpr ({op = Neq; _} as r) -> (
+    match (typecheck_expr r.left env, typecheck_expr r.right env) with
+    | (Some left', Some right') -> (
+        match (prop_of_prog left', prop_of_prog right') with
+        | (Integer, Double)
+        | (Double, Integer) -> Some (BinExpr {r with
+            prop = Bool;
+            left = left';
+            right = right';
+          })
+        | (t1, t2) -> if t1 == t2 then (Some (BinExpr {r with
+              prop = Bool;
+              left = left';
+              right = right';
+            })
+          ) else (prerr_string
+              (string_of_positions r.pos 
+                ^ ": expected bool operands, but found "
+                ^ string_of_type t1 ^ ", "
+                ^ string_of_type t2 ^ "\n");
+              None
+          )
+      )
     | _ -> None
   )
   | BinExpr ({op = BoolOr; _} as r) 
@@ -248,99 +289,90 @@ and typecheck_expr(p : unit prog)(env : binding list) : ty prog option =
               ^ ": expected bool operands, but found "
               ^ string_of_type t1 ^ ", "
               ^ string_of_type t2 ^ "\n");
-          None
-    )
-    | _ -> None
-  )
-  | BinExpr ({op = Greater; _} as r)
-  | BinExpr ({op = Less; _} as r)
-  | BinExpr ({op = Geq; _} as r)
-  | BinExpr ({op = Leq; _} as r) -> (
-    match (typecheck_expr r.left env, typecheck_expr r.right env) with
-    | (Some left', Some right') -> (
-        match (prop_of_prog left', prop_of_prog right') with
-        | (Double, Double)
-        | (Integer, Double)
-        | (Double, Integer)
-        | (Integer, Integer) -> Some (BinExpr {r with
-            prop = Bool;
-            left = left';
-            right = right';
-          })
-        | (t1, t2) -> prerr_string
-            (string_of_positions r.pos 
-              ^ ": expected bool operands, but found "
-              ^ string_of_type t1 ^ ", "
-              ^ string_of_type t2 ^ "\n");
-          None
-    )
-    | _ -> None
-  )
-  | BinExpr ({op = Equal; _} as r)
-  | BinExpr ({op = Neq; _} as r) -> (
-    match (typecheck_expr r.left env, typecheck_expr r.right env) with
-    | (Some left', Some right') -> (
-        match (prop_of_prog left', prop_of_prog right') with
-        | (Integer, Double)
-        | (Double, Integer) -> Some (BinExpr {r with
-            prop = Bool;
-            left = left';
-            right = right';
-          })
-        | (t3, t4) -> if (t3 == t4) then Some (BinExpr {r with
-          prop = Bool;
-          left = left';
-          right = right';
-        }) else
-          None
-    )
+            None
+      )
     | _ -> None
   )
   | BinExpr ({op = Con; _} as r) -> (
     match (typecheck_expr r.left env, typecheck_expr r.right env) with
     | (Some left', Some right') -> (
         match (prop_of_prog left', prop_of_prog right') with
-        | (t3, List t4) -> if (t3 == t4) then Some (BinExpr {r with
-          prop = List t4;
-          left = left';
-          right = right';
-        }) else None
-        | _ -> None
-    )
+        | (t1, List t2) -> if (t1 == t2 || t2 == EmptyList) then (Some (BinExpr {r with
+              prop = List t1;
+              left = left';
+              right = right';
+            })
+          ) else (prerr_string
+              (string_of_positions r.pos 
+                ^ ": expected bool operands, but found "
+                ^ string_of_type t1 ^ ", "
+                ^ string_of_type t2 ^ "\n");
+              None
+          )
+        | (t1, t2) -> prerr_string
+            (string_of_positions r.pos 
+              ^ ": expected 't :: List 't operands, but found "
+              ^ string_of_type t1 ^ ", "
+              ^ string_of_type t2 ^ "\n");
+            None
+      )
     | _ -> None
   )
   | BinExpr ({op = Concat; _} as r) -> (
     match (typecheck_expr r.left env, typecheck_expr r.right env) with
     | (Some left', Some right') -> (
         match (prop_of_prog left', prop_of_prog right') with
-        | (List t3, List t4) -> if (t3 == t4) then Some (BinExpr {r with
-          prop = List t4;
-          left = left';
-          right = right';
-        }) else None
-        | _ -> None
-    )
+        | (List t1, List t2) -> if (t1 == t2 || t1 == EmptyList || t2 == EmptyList) then (Some (BinExpr {r with
+              prop = List t1;
+              left = left';
+              right = right';
+            })
+          ) else (prerr_string
+              (string_of_positions r.pos 
+                ^ ": expected bool operands, but found "
+                ^ string_of_type t1 ^ ", "
+                ^ string_of_type t2 ^ "\n");
+              None
+          )
+        | (t1, t2) -> prerr_string
+            (string_of_positions r.pos 
+              ^ ": expected List 't ++ List 't operands, but found "
+              ^ string_of_type t1 ^ ", "
+              ^ string_of_type t2 ^ "\n");
+            None
+      )
     | _ -> None
   )
-  | IfExpr (r) -> (
-    match (typecheck_expr r.guard env) with
-    | (Some guard) -> (
-        match (prop_of_prog guard) with
-        | (Bool) -> (
-          match (typecheck_expr r.conseq env, typecheck_expr r.alter env) with
-          | (Some cons, Some alt) -> (
-              match (prop_of_prog cons, prop_of_prog alt) with
-              | (t3, t4) -> if (t3 == t4) then Some (IfExpr {r with
-                prop = t3;
-                guard = guard;
-                conseq = cons;
-                alter = alt;
-              }) else None
+  | IfExpr r -> (
+    match typecheck_expr r.guard env with
+    | Some guard' -> (
+        match prop_of_prog guard' with
+        | Bool -> (
+            match (typecheck_expr r.conseq env, typecheck_expr r.alter env) with
+            | (Some conseq', Some alter') -> (
+                match (prop_of_prog conseq', prop_of_prog alter') with
+                | (t1, t2) -> if t1 == t2 then (Some (IfExpr {r with
+                    prop = t1;
+                    guard = guard';
+                    conseq = conseq';
+                    alter = alter';
+                  }) 
+                ) else (prerr_string
+                    (string_of_positions r.pos 
+                      ^ ": expected then 't else 't, but found "
+                      ^ string_of_type t1 ^ ", "
+                      ^ string_of_type t2 ^ "\n");
+                    None
+                )
+              )
+            | _ -> None
           )
-          | _ -> None
-        )
-        | _ -> None
-    )
+        | t -> prerr_string
+            (string_of_positions r.pos 
+              ^ ": expected Bool guard in if-expr, but found "
+              ^ string_of_type t ^ "\n");
+            None
+      )
     | _ -> None
   )
   | _ -> None
